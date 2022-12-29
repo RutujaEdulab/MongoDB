edb43a516d4c7635d3262050334da22313781cf9
RutujaEdulab <rutuja@edulab.in>
2022-12-29 15:02:53 +0530

    rp-feat-MongodbConnection

--- insert.js
+++ insert.js
@@ -1,7 +1,11 @@
 const dbconnect = require('./Connectivity');
 
+
+//Insert Code
 const insert = async () => {
-    const db = await dbconnect();//Here throw Promise.
+    const db = await dbconnect();//Here return Promise.
+
+    //single record inserted
     const result = await db.insert(
         { Fname: "Aruna", Lname: "Waghmare", Address: "Kandivali", PhoneNo: 457889541697 }
     );
@@ -11,5 +15,20 @@ const insert = async () => {
     else {
         console.log("Sorry Your data Is Not Inserted");
     }
+
+    //This Code for inserting multiple records
+    // const result = await db.insert(
+    //    [ { Fname: "Aruna", Lname: "Waghmare", Address: "Kandivali", PhoneNo: 457889541697 },
+    //     { Fname: "Ruhi", Lname: "Sharma", Address: "Malad", PhoneNo:4587123695 },
+    //     
+    // ]
+
+    // );
+    // if (result.acknowledged == true) {
+    //     console.log("Data Inserted Successfully");
+    // }
+    // else {
+    //     console.log("Sorry Your data Is Not Inserted");
+    // }
 }
 insert();
